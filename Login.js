36c3e447ba6ab00504a344b7398a00c5086fc36b
YUJINKIM <yujin7621@me.com>
2018-06-12 19:49:08 +0900

    [feature/auto-login] (#1) 자동로그인 기능 구현

--- Login.js
+++ Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { 
+import {
   StyleSheet, Text ,View, Image, TextInput, Button,
    StatusBar, SafeAreaView, Keyboard, KeyboardAvoidingView,
   AsyncStorage, Alert, ActivityIndicator
@@ -22,8 +22,13 @@ export default class Login extends Component {
         isLoading: false
       }
     }
-    componentDidMount() {
+    async componentDidMount() {
       this.forestApi = new ForestApi('http://localhost:3000');
+      let id = await AsyncStorage.getItem('userid');
+      let pw = await AsyncStorage.getItem('userpw');
+      if(id !== null && pw !== null){
+        this.runLogInProcess(id, pw);
+      }
     }
 
     render() {
@@ -35,87 +40,31 @@ export default class Login extends Component {
       }else{
         logInContainer = (
           <View >
-                <TextInput style={{fontSize: 20, padding: 10}} placeholder='아이디(학번) 입력' keyboardType='default' 
+                <TextInput style={{fontSize: 20, padding: 10}} placeholder='아이디(학번) 입력' keyboardType='default'
                 returnKeyType='next' autocorrect={ false } onSubmitEditing={ () => this.refs.password.focus() }
                 onChangeText={(text)=>{this.setState({idInput: text})}}>
                 </TextInput>
-                <TextInput style={{fontSize: 20, padding: 10}} placeholder='비밀번호 입력' 
+                <TextInput style={{fontSize: 20, padding: 10}} placeholder='비밀번호 입력'
                 returnkeyType='go' ref={ 'password' } secureTextEntry={ true } autocorrect={ false }
                 onChangeText={(text)=>{this.setState({pwInput: text})}}>
                 </TextInput>
                 <Button
                   title="Log In"
-                  onPress={async()=>{ 
-                   try{
-                    this.setState({isLoading: true});
+                  onPress={()=>{
                     let id = this.state.idInput.replace(/\s/g,'');
                     let pw = this.state.pwInput.replace(/\s/g,'');
-                    if(id.length <= 0 || pw.length <= 0){
-                      alert("학번 또는 비밀번호가 입력되지 않았습니디.");
-                      this.setState({isLoading: false});
-                    }else{
-                      let response = await this.forestApi.login(id, pw);
-                      if(response.ok){
-                        let data = await response.json();
-                        await AsyncStorage.setItem('CredentialOld', data['credential-old']);
-                        await AsyncStorage.setItem('CredentialNew', data['credential-new']);
-                        await AsyncStorage.setItem('CredentialNewToken', data['credential-new-token']);
-                        this.setState({isLoading: false});
-                        this.props.navigation.navigate('Main');
-                      }else if(response.status == 400){
-                        this.setState({isLoading: false});
-                        setTimeout(() => {
-                          Alert.alert(
-                            '로그인 오류',
-                            '입력된 학번(아이디) 또는 비밀번호가 올바르지 않습니다.',
-                            [
-                              {text: '확인', onPress: () => console.log('OK Pressed')},
-                            ],
-                            { cancelable: false }
-                          );
-                        }, 10);
-                        
-                      }else if(response.status == 401){
-                        this.setState({isLoading: false});
-                        setTimeout(()=>{
-                          Alert.alert(
-                            '로그인 실패',
-                            '입력된 학번(아이디) 또는 비밀번호를 다시한번 확인하세요.',
-                            [
-                              {text: '확인', onPress: () => console.log('OK Pressed')},
-                            ],
-                            { cancelable: false }
-                          )
-                        }, 10);
-                      }else{
-                        this.setState({isLoading: false});
-                      }
-                    }
-                   }catch(err){
-                    this.setState({isLoading: false});
-                    console.log(err);
-                    setTimeout(()=>{
-                      Alert.alert(
-                        '로그인 오류',
-                        err,
-                        [
-                          {text: '확인', onPress: () => console.log('OK Pressed')},
-                        ],
-                        { cancelable: false }
-                      )
-                    }, 10);
-                    
-                   }
+                    this.runLogInProcess(id, pw);
                   }}/>
               </View>
         )
       }
+
       return (
         <SafeAreaView style={ styles.container }>
           <KeyboardAvoidingView behavior='padding' style={ styles.container }>
             <View style={ styles.container }>
               <View style={ styles.title_container }>
-                <Image style={ styles.skhu_logo } 
+                <Image style={ styles.skhu_logo }
                   source={ require('./assets/login/skhu_logo.png') }>
                 </Image>
                 <Text style={ styles.title_text }>App Title</Text>
@@ -124,7 +73,7 @@ export default class Login extends Component {
               {logInContainer}
               <View style={ styles.footer }>
                   <Text style={ styles.info }>성공회대학교 종합정보시스템{"\n"}계정으로 로그인.</Text>
-                  <Text style={ styles.copy }>(C)2018-Present Sleey OWL</Text>
+                  <Text style={ styles.copy }>(C)2018-Present Sleepy OWL</Text>
                 <Image style={ styles.sowl_logo } source={ require('./assets/login/Sowl_Logo.png') }>
                 </Image>
                 </View>
@@ -134,8 +83,71 @@ export default class Login extends Component {
         </SafeAreaView>
       );
     }
+    async runLogInProcess(id, pw){
+      try{
+       this.setState({isLoading: true});
+       if(id.length <= 0 || pw.length <= 0){
+         alert("학번 또는 비밀번호가 입력되지 않았습니디.");
+         this.setState({isLoading: false});
+       }else{
+         let response = await this.forestApi.login(id, pw);
+         if(response.ok){
+           let data = await response.json();
+           await AsyncStorage.setItem('CredentialOld', data['credential-old']);
+           await AsyncStorage.setItem('CredentialNew', data['credential-new']);
+           await AsyncStorage.setItem('CredentialNewToken', data['credential-new-token']);
+           await AsyncStorage.setItem('userid', this.state.idInput);
+           await AsyncStorage.setItem('userpw', this.state.pwInput);
+           this.setState({isLoading: false});
+           this.props.navigation.navigate('Main');
+         }else if(response.status == 400){
+           this.setState({isLoading: false});
+           setTimeout(() => {
+             Alert.alert(
+               '로그인 오류',
+               '입력된 학번(아이디) 또는 비밀번호가 올바르지 않습니다.',
+               [
+                 {text: '확인', onPress: () => console.log('OK Pressed')},
+               ],
+               { cancelable: false }
+             );
+           }, 10);
+
+         }else if(response.status == 401){
+           this.setState({isLoading: false})
+           ;
+           setTimeout(()=>{
+             Alert.alert(
+               '로그인 실패',
+               '입력된 학번(아이디) 또는 비밀번호를 다시한번 확인하세요.',
+               [
+                 {text: '확인', onPress: () => console.log('OK Pressed')},
+               ],
+               { cancelable: false }
+             )
+           }, 10);
+         }else{
+           this.setState({isLoading: false});
+         }
+       }
+      }catch(err){
+       this.setState({isLoading: false});
+       console.log(err);
+       setTimeout(()=>{
+         Alert.alert(
+           '로그인 오류',
+           err,
+           [
+             {text: '확인', onPress: () => console.log('OK Pressed')},
+           ],
+           { cancelable: false }
+         )
+       }, 10);
+
+      }
+    }
   }
-  
+
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -204,4 +216,3 @@ export default class Login extends Component {
       marginTop: 30
     }
   });
-  
