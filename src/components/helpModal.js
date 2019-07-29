89d070a783ef2c58eedb1dec9f815c6e41f27d94
sukso96100 <sukso96100@gmail.com>
2019-07-29 15:11:32 +0900

    [feature/better-error]로그인 전/후 옵션, 메일 문의시 로그첨부(#75)

--- src/components/helpModal.js
+++ src/components/helpModal.js
@@ -26,6 +26,41 @@ export class HelpModal extends Component{
   }
 
   render(){
+    let sections = [
+      {title: '공지사항 및 도움말 확인', data: [
+        {label: '공지사항 확인(페이스북 페이지)', icon: 'facebook-box', onPress: ()=>{
+          Linking.openURL('https://fb.me/SKHUsMobileApp');
+        }},
+        {label: '자주 묻는 질문', icon: 'help-circle-outline', onPress: ()=>{
+          Linking.openURL('https://github.com/s-owl/skhus/wiki/FAQs');
+        }}
+      ]},
+      {title: '문의하거나 오류 보고하기', data: [
+        {label: '페이스북 메신저로 문의', icon: 'facebook-messenger', onPress: ()=>{
+          Linking.openURL('https://m.me/SKHUsMobileApp');
+        }},
+        {label: '이메일로 문의', icon: 'email-outline' ,onPress: ()=>{
+          Linking.openURL(`mailto:s.owl.contact@gmail.com?body=${this.state.errorMsg}`);
+        }},
+        {label: 'GitHub에 이슈 등록', icon: 'github-circle', onPress: ()=>{
+          Linking.openURL('https://github.com/s-owl/skhus/issues');
+        }}
+      ]}
+    ];
+    if(this.state.isDuringLogin){
+      sections = [
+        ...sections,
+        {title: '계정 복구', data: [
+          {label: '계정 찾기', icon: 'account-search' ,onPress: ()=>{
+            Linking.openURL('http://sid.skhu.ac.kr/SID03/SID0301');
+          }},
+          {label: '비밀번호 복구', icon: 'textbox-password', onPress: ()=>{
+            Linking.openURL('http://sid.skhu.ac.kr/SID02/SID0201');
+          }}
+        ]
+        }
+      ];
+    }
     return(
           
       <Modal
@@ -49,35 +84,7 @@ export class HelpModal extends Component{
                   <Text style={{fontWeight: 'bold'}}>{title}</Text>
                 </CardItem>
               )}
-              sections={[
-                {title: '공지사항 및 도움말 확인', data: [
-                  {label: '공지사항 확인(페이스북 페이지)', icon: 'facebook-box', onPress: ()=>{
-                    Linking.openURL('https://fb.me/SKHUsMobileApp');
-                  }},
-                  {label: '자주 묻는 질문', icon: 'help-circle-outline', onPress: ()=>{
-                    Linking.openURL('https://github.com/s-owl/skhus/wiki/FAQs');
-                  }}
-                ]},
-                {title: '문의하거나 오류 보고하기', data: [
-                  {label: '페이스북 메신저로 문의', icon: 'facebook-messenger', onPress: ()=>{
-                    Linking.openURL('https://m.me/SKHUsMobileApp');
-                  }},
-                  {label: '이메일로 문의', icon: 'email-outline' ,onPress: ()=>{
-                    Linking.openURL('mailto:s.owl.contact@gmail.com');
-                  }},
-                  {label: 'GitHub에 이슈 등록', icon: 'github-circle', onPress: ()=>{
-                    Linking.openURL('https://github.com/s-owl/skhus/issues');
-                  }}
-                ]},
-                {title: '계정 복구', data: [
-                  {label: '계정 찾기', icon: 'account-search' ,onPress: ()=>{
-                    Linking.openURL('http://sid.skhu.ac.kr/SID03/SID0301');
-                  }},
-                  {label: '비밀번호 복구', icon: 'textbox-password', onPress: ()=>{
-                    Linking.openURL('http://sid.skhu.ac.kr/SID02/SID0201');
-                  }}
-                ]},
-              ]}
+              sections={sections}
               keyExtractor={(item, index) => item + index}
             />
           </ScrollView>
