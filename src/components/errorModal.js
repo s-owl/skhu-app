b09116c4ea786d388aa65cac97d5b8899fc16b24
sukso96100 <sukso96100@gmail.com>
2019-07-29 15:09:45 +0900

    [feature/better-error]오류 유형 추가  및  오류별 액션 추가 (#75)

--- src/components/errorModal.js
+++ src/components/errorModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Modal, Text, View, ScrollView} from 'react-native';
+import {Modal, Text, View, ScrollView, Clipboard} from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import {CardItem} from './components';
 import {HelpModal} from './helpModal';
@@ -21,6 +21,23 @@ export class ErrorModal extends Component{
         }}
       ]
     },
+
+    wrongLogin: {
+      icon: 'alert-circle-outline',
+      title: '틀린 로그인 정보',
+      desc: '아이디(학번) 또는 비밀번호 입력란이 비어 있거나, 비밀번호가 8자리 미만입니다.\n'
+        + '신/편입생 신규 계정에 종합정보시스템이 부여하는 초기 비밀번호는 s + (주민번호 뒤 7자리)로, 총 8자리 이며, 비밀번호 변경시 9자리 이상을 요구합니다.\n'
+        + '8자리 미만 비밀번호 사용 시 PC 에서 종합정보시스템에 접속하여 비밀번호 변경 후 사용해 주세요.\n'
+        + '비밀번호 변경에 문제가 있는 경우 성공회대학교 전자계산소에 문의하시기 바랍니다.',
+      buttons: [
+        {label: '다시 시도', onPress:()=>{
+          NavigationService.reset('Login');
+        }},
+        {label: '도움 받기', onPress:()=>{
+          this.helpModal.current.open(undefined, true);
+        }}
+      ]
+    },
   
     loginError: {
       icon: 'alert-circle-outline',
@@ -32,20 +49,33 @@ export class ErrorModal extends Component{
       buttons: [
         {label: '다시 시도', onPress:()=>{
           NavigationService.reset('Login');
+        }},
+        {label: '오류 정보 복사', onPress:()=>{
+          Clipboard.setString(this.state.desc);
+        }},
+        {label: '도움 받기', onPress:()=>{
+          this.helpModal.current.open(this.state.desc, true);
         }}
       ]
     },
   
     netError: {
-      icon: 'alert-circle-outline',
-      title: '로그인 오류',
-      desc: '1. 네트워크 연결상태를 확인하세요.\n\n'
-        + '2. forest.skhu.ac.kr 및 sam.skhu.ac.kr 접속이 정상인지 확인하세요.\n'
-        + '3. 네트워크 연결 또는 SKHU\'s 서버에 문제가 있을 수 있습니다. 네트워크 연결 문제가 아닌 경우 SKHU\'s 개발팀에 보고해 주세요.\n\n'
+      icon: 'web',
+      title: '네트워크 오류',
+      desc: '1. 네트워크 연결상태를 확인하세요. 속도가 느려 시간 초과로 오류가 발생했을 수도 있습니다.\n\n'
+        + '2. forest.skhu.ac.kr 및 sam.skhu.ac.kr 접속이 정상인지 확인하세요.\n\n'
+        + '3. 학번이나 비밀번호가 틀렸을 수도 있습니다. 다시 한번 확인하세요.\n\n'
+        + '4. SKHU\'s 서버에 문제가 있을 수 있습니다. 네트워크 연결 문제가 아닌 경우 SKHU\'s 개발팀에 보고해 주세요.\n\n'
         + '추가정보:\n',
       buttons: [
         {label: '다시 시도', onPress:()=>{
           NavigationService.reset('Login');
+        }},
+        {label: '오류 정보 복사', onPress:()=>{
+          Clipboard.setString(this.state.desc);
+        }},
+        {label: '도움 받기', onPress:()=>{
+          this.helpModal.current.open(this.state.desc, true);
         }}
       ]
     }
