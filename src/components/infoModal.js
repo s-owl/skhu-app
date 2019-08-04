aff32abfbc5bd7eb3db5df3fe50d0458971f7f87
tpdns90321 <tpdns9032100@gmail.com>
2019-08-04 15:08:32 +0900

    [feature/about-update] 작은 화면에서의 레이아웃 개선

--- src/components/infoModal.js
+++ src/components/infoModal.js
@@ -10,8 +10,8 @@ export function InfoModal(props) {
       <Modal
         animationType="slide"
         visible={props.visible}>
-        <View style={{paddingTop: 40, padding: 16}}>
-          <View style={{padding: 32, alignItems: 'center'}}>
+        <View style={{paddingTop: 50, padding: 16}}>
+          <View style={{padding: 8, alignItems: 'center'}}>
             <MaterialCommunityIcons name={props.icon} size={40} style={{padding: 8}}/>
             <Text style={{fontWeight: 'bold', padding: 8, fontSize: 20}}>{props.title}</Text>
           </View>
