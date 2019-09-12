c9bc0388442b5af02df57115b0c72a3e795c30e0
sukso96100 <sukso96100@gmail.com>
2019-09-12 19:56:42 +0900

    [feature/ota-info] OTA 정보 표시 추가

--- src/config.js
+++ src/config.js
@@ -4,4 +4,5 @@ export default class BuildConfigs{
     //static API_SERVER_ADDR = 'https://skhusapp.sleepy-owl.com';
     static OPENWEATHERMAP_API_KEY = '8834d9ceca79cf8f705f892add715f7e'; // OpenWeatherMap API KEY
     static primaryColor = '#569f59';
+    static OtaDeployedAt = '2019.09.13'
 }
