e4fb148fb1ea8002b36442dde4c2f1d44f8381f5
sukso96100 <sukso96100@gmail.com>
2020-03-22 20:35:09 +0900

    [feature/react-nav5] 설정화면 적용작업 (#144)

--- src/screens/settings.js
+++ src/screens/settings.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useRef} from 'react';
 import { 
   Switch, SafeAreaView, SectionList, AsyncStorage, Button, Platform,
-  View, Alert, KeyboardAvoidingView, InputAccessoryView
+  View, Alert, KeyboardAvoidingView, InputAccessoryView, TextInput
 } from 'react-native';
 import ListItem from '../components/listitem';
 import * as SecureStore from 'expo-secure-store';
 import SnackBar from 'react-native-snackbar-component';
-import {ThemedText, ThemedTextInput} from '../components/components';
+import {ThemedText} from '../components/components';
+import {useTheme} from '@react-navigation/native';
 
 export class Settings extends Component {
   constructor(props){
@@ -100,247 +101,234 @@ export class Settings extends Component {
   }
 }
 
-export class PinRecovery extends Component{
-  static navigationOptions = ({navigation, navigationOptions}) => {
-    const {params} = navigation.state;
-  
-    return {
-      title: 'PIN 복구',
-    };
+export function PinRecovery(props){
+  const [newPin, setNewPin] = useState('');
+  const [newPinCheck, setNewPinCheck] = useState('');
+  const [userpw, setUserpw] = useState('');
+  const [msg, setMsg] = useState('');
+  const [snackbar, setSnackbar] = useState(false);
+  const {colors} = useTheme();
+
+  const newPinInput = useRef();
+  const newPinCheckInput = useRef();
+  const userpwInput = useRef();
+
+  const showSnackbar = (msg) => {
+    setMsg(msg);
+    setSnackbar(true);
+    setTimeout(()=>{
+      setMsg('');
+      setSnackbar(false);
+    }, 3000);
   };
 
-  constructor(props){
-    super(props);
-    this.state = {
-      newPin: '',
-      newPinCheck: '',
-      userpw: '',
-      msg: '',
-      snackbar: false
-    };
-    this.newPin = React.createRef();
-    this.newPinCheck = React.createRef();
-    this.userpw = React.createRef();
-  }
-
-  showSnackbar(msg){
-    this.setState({msg: msg, snackbar: true});
-    setTimeout(()=>{
-      this.setState({msg: '', snackbar: false});
-    }, 3000);
-  }
-
-  render(){
-    const idToNewPinCheck = 'idToNewPinCheck';
-    const idToUserpw = 'idToUserpw';
-    const idRecover = 'idRecover';
-    let keyboardToolbar = (Platform.OS == 'ios') ? (
-      <View>
-        <InputAccessoryView nativeID={idToNewPinCheck}>
-          <Button
-            onPress={() => this.newPinCheck.current.focus()}
-            title="새 PIN 확인(6자) 입력"
-          />
-        </InputAccessoryView>
-        <InputAccessoryView nativeID={idToUserpw}>
-          <Button
-            onPress={() => this.userpw.current.focus()}
-            title="로그인 비밀번호 입력"
-          />
-        </InputAccessoryView>
-        <InputAccessoryView nativeID={idRecover}>
-          <Button
-            onPress={() => this.recoverPin()}
-            title="PIN 복구"
-          />
-        </InputAccessoryView>
-      </View>) : (<View></View>);
-    return(
-      <KeyboardAvoidingView behavior="padding" enabled style={{flex: 1}}>
-        <View style={{flex: 1}}>
-          <ListItem>
-            <ThemedTextInput placeholder='새 PIN 입력(6자)'  returnkeyType='next' keyboardType='number-pad'
-              maxLength={6} secureTextEntry={ true } autocorrect={ false } ref={this.newPin}
-              onSubmitEditing={ () => this.newPinCheck.current.focus() }
-              onChangeText={(value)=>this.setState({newPin: value})}
-              inputAccessoryViewID={idToNewPinCheck}/>
-          </ListItem>
-          <ListItem>
-            <ThemedTextInput placeholder='새 PIN 확인(6자)'  returnkeyType='next' keyboardType='number-pad'
-              maxLength={6} secureTextEntry={ true } autocorrect={ false } ref={this.newPinCheck}
-              onSubmitEditing={ () => this.userpw.current.focus() }
-              onChangeText={(value)=>this.setState({newPinCheck: value})}
-              inputAccessoryViewID={idToUserpw}/>
-          </ListItem>
-          <ListItem>
-            <ThemedTextInput placeholder='로그인 비밀번호 입력'  returnkeyType='go'
-              secureTextEntry={ true } autocorrect={ false } ref={this.userpw}
-              onSubmitEditing={()=>this.recoverPin()}
-              onChangeText={(value)=>this.setState({userpw: value})}
-              inputAccessoryViewID={idRecover}/>
-          </ListItem>
-        </View>
-        <View style={{flex: 0, flexDirection: 'row', backgroundColor: 'white',
-          height: 50, width: '100%', marginBottom: 16}}>
-          <ListItem style={{flex: 1, alignItems: 'center'}}
-            onPress={()=>this.props.navigation.goBack()}>
-            <ThemedText>취소</ThemedText>
-          </ListItem>
-          <ListItem style={{flex: 1, alignItems: 'center'}}
-            onPress={()=>this.recoverPin()}>
-            <ThemedText>복구</ThemedText>
-          </ListItem>
-        </View>
-        <SnackBar visible={this.state.snackbar} textMessage={this.state.msg}/>
-        {keyboardToolbar}
-      </KeyboardAvoidingView>
-    );
-  }
-
-  async recoverPin(){
-    if(this.state.newPin.length == 6 && this.state.newPinCheck.length == 6){
-      if(this.state.newPin == this.state.newPinCheck){
+  const recoverPin = async() => {
+    if(newPin.length == 6 && newPinCheck.length == 6){
+      if(newPin == newPinCheck){
         const userpw = await SecureStore.getItemAsync('userpw');
-        if(userpw == this.state.userpw){
-          await SecureStore.setItemAsync('localAuthPin', this.state.newPin);
+        if(userpw == userpw){
+          await SecureStore.setItemAsync('localAuthPin', newPin);
           Alert.alert(
             'PIN 복구',
             'PIN 이 복구되었습니다.',
             [
-              {text: '확인', onPress: ()=>this.props.navigation.goBack()}
+              {text: '확인', onPress: ()=>props.navigation.goBack()}
             ]);
         }else{
-          this.showSnackbar('틀린 로그인 비밀번호 입니다.');
+          showSnackbar('틀린 로그인 비밀번호 입니다.');
         }
       }else{
-        this.showSnackbar('새 PIN 과 PIN 확인 값이 다릅니다.');
+        showSnackbar('새 PIN 과 PIN 확인 값이 다릅니다.');
       }
     }else{
-      this.showSnackbar('PIN은 6자리여야 합니다.');
+      showSnackbar('PIN은 6자리여야 합니다.');
     }
-  }
+  };
+
+  const idToNewPinCheck = 'idToNewPinCheck';
+  const idToUserpw = 'idToUserpw';
+  const idRecover = 'idRecover';
+  let keyboardToolbar = (Platform.OS == 'ios') ? (
+    <View>
+      <InputAccessoryView nativeID={idToNewPinCheck}>
+        <Button
+          onPress={() =>newPinCheckInput.current.focus()}
+          title="새 PIN 확인(6자) 입력"
+        />
+      </InputAccessoryView>
+      <InputAccessoryView nativeID={idToUserpw}>
+        <Button
+          onPress={() =>userpwInput.current.focus()}
+          title="로그인 비밀번호 입력"
+        />
+      </InputAccessoryView>
+      <InputAccessoryView nativeID={idRecover}>
+        <Button
+          onPress={() => recoverPin()}
+          title="PIN 복구"
+        />
+      </InputAccessoryView>
+    </View>) : (<View></View>);
+  return(
+    <KeyboardAvoidingView behavior="padding" enabled style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <ListItem>
+          <TextInput style={{color: colors.text}} placeholder='새 PIN 입력(6자)'
+            returnkeyType='next' keyboardType='number-pad'
+            maxLength={6} secureTextEntry={ true } autocorrect={ false } ref={newPinInput}
+            onSubmitEditing={ () => newPinCheckInput.current.focus() }
+            onChangeText={(value)=>setNewPin(value)}
+            inputAccessoryViewID={idToNewPinCheck}/>
+        </ListItem>
+        <ListItem>
+          <TextInput style={{color: colors.text}} placeholder='새 PIN 확인(6자)'
+            returnkeyType='next' keyboardType='number-pad'
+            maxLength={6} secureTextEntry={ true } autocorrect={ false } ref={newPinCheckInput}
+            onSubmitEditing={ () => userpwInput.current.focus() }
+            onChangeText={(value)=>setNewPinCheck(value)}
+            inputAccessoryViewID={idToUserpw}/>
+        </ListItem>
+        <ListItem>
+          <TextInput style={{color: colors.text}} placeholder='로그인 비밀번호 입력'
+            returnkeyType='go'
+            secureTextEntry={ true } autocorrect={ false } ref={userpwInput}
+            onSubmitEditing={()=>recoverPin()}
+            onChangeText={(value)=>setUserpw(value)}
+            inputAccessoryViewID={idRecover}/>
+        </ListItem>
+      </View>
+      <View style={{flex: 0, flexDirection: 'row', backgroundColor: 'white',
+        height: 50, width: '100%', marginBottom: 16}}>
+        <ListItem style={{flex: 1, alignItems: 'center'}}
+          onPress={()=>props.navigation.goBack()}>
+          <ThemedText>취소</ThemedText>
+        </ListItem>
+        <ListItem style={{flex: 1, alignItems: 'center'}}
+          onPress={()=>recoverPin()}>
+          <ThemedText>복구</ThemedText>
+        </ListItem>
+      </View>
+      <SnackBar visible={snackbar} textMessage={msg}/>
+      {keyboardToolbar}
+    </KeyboardAvoidingView>
+  );
+  
+
+  
 }
 
-export class ChangePin extends Component{
-  static navigationOptions = ({navigation, navigationOptions}) => {
-    const {params} = navigation.state;
-  
-    return {
-      title: 'PIN 변경',
-    };
-  };
-  constructor(props){
-    super(props);
-    this.state = {
-      newPin: '',
-      newPinCheck: '',
-      currentPin: '',
-      msg: '',
-      snackbar: false
-    };
+export function ChangePin(props){
+  const [newPin, setNewPin] = useState('');
+  const [newPinCheck, setNewPinCheck] = useState('');
+  const [currentPin, setCurrentPin] = useState('');
+  const [msg, setMsg] = useState('');
+  const [snackbar, setSnackbar] = useState(false);
+  const {colors} = useTheme();
 
-    this.newPin = React.createRef();
-    this.newPinCheck = React.createRef();
-    this.currentPin = React.createRef();
-  }
+  const newPinInput = useRef();
+  const newPinCheckInput = useRef();
+  const currentPinInput = useRef();
 
-  showSnackbar(msg){
-    this.setState({msg: msg, snackbar: true});
+  const showSnackbar = (msg) => {
+    setMsg(msg);
+    setSnackbar(true);
     setTimeout(()=>{
-      this.setState({msg: '', snackbar: false});
+      setMsg('');
+      setSnackbar(false);
     }, 3000);
-  }
+  };
 
-  render(){
-    const idToNewPin = 'idToNewPin';
-    const idToNewPinCheck = 'idToNewPinCheck';
-    const idChange = 'idChange';
-
-    let keyboardToolbar = (Platform.OS == 'ios') ? (
-      <View>
-        <InputAccessoryView nativeID={idToNewPin}>
-          <Button
-            onPress={() => this.newPin.current.focus()}
-            title="새 PIN(6자) 입력"
-          />
-        </InputAccessoryView>
-        <InputAccessoryView nativeID={idToNewPinCheck}>
-          <Button
-            onPress={() => this.newPinCheck.current.focus()}
-            title="새 PIN 확인(6자) 입력"
-          />
-        </InputAccessoryView>
-        <InputAccessoryView nativeID={idChange}>
-          <Button
-            onPress={() => this.changePin()}
-            title="PIN 변경"
-          />
-        </InputAccessoryView>
-      </View>) : (<View></View>);
-    return(
-      <KeyboardAvoidingView behavior="padding" enabled style={{flex: 1}}>
-        <View style={{flex: 1}}>
-          <ListItem>
-            <ThemedTextInput placeholder='기존 PIN 입력'  returnkeyType='next' keyboardType='number-pad'
-              secureTextEntry={ true } autocorrect={ false } ref={this.currentPin}
-              onSubmitEditing={ () => this.newPin.current.focus() } maxLength={6}
-              onChangeText={(value)=>this.setState({currentPin: value})}
-              inputAccessoryViewID={idToNewPin}/>
-          </ListItem>
-          <ListItem>
-            <ThemedTextInput placeholder='새 PIN 입력(6자)'  returnkeyType='next' keyboardType='number-pad'
-              maxLength={6} secureTextEntry={ true } autocorrect={ false } ref={this.newPin}
-              onSubmitEditing={ () => this.newPinCheck.current.focus() }
-              onChangeText={(value)=>this.setState({newPin: value})}
-              inputAccessoryViewID={idToNewPinCheck}/>
-          </ListItem>
-          <ListItem>
-            <ThemedTextInput placeholder='새 PIN 확인(6자)'  returnkeyType='go' keyboardType='number-pad'
-              maxLength={6} secureTextEntry={ true } autocorrect={ false } ref={this.newPinCheck}
-              onSubmitEditing={ () => this.changePin() }
-              onChangeText={(value)=>this.setState({newPinCheck: value})}
-              inputAccessoryViewID={idChange}/>
-          </ListItem>
-        </View>
-        <View style={{flex: 0, flexDirection: 'row', backgroundColor: 'white',
-          height: 50, width: '100%', marginBottom: 16}}>
-          <ListItem style={{flex: 1, alignItems: 'center'}}
-            onPress={()=>this.props.navigation.goBack()}>
-            <ThemedText>취소</ThemedText>
-          </ListItem>
-          <ListItem style={{flex: 1, alignItems: 'center'}}
-            onPress={()=>this.changePin()}>
-            <ThemedText>복구</ThemedText>
-          </ListItem>
-        </View>
-        <SnackBar visible={this.state.snackbar} textMessage={this.state.msg}/>
-        {keyboardToolbar}
-      </KeyboardAvoidingView>
-    );
-  }
-
-  async changePin(){
-    if(this.state.currentPin.length == 6 &&
-      this.state.newPin.length == 6 && 
-      this.state.newPinCheck.length == 6){
-      if(this.state.newPin == this.state.newPinCheck){
+  const changePin = async()=>{
+    if(currentPin.length == 6 &&
+      newPin.length == 6 && 
+      newPinCheck.length == 6){
+      if(newPin == newPinCheck){
         const currentPin = await SecureStore.getItemAsync('localAuthPin');
-        if(currentPin == this.state.currentPin){
-          await SecureStore.setItemAsync('localAuthPin', this.state.newPin);
+        if(currentPin == currentPin){
+          await SecureStore.setItemAsync('localAuthPin', newPin);
           Alert.alert(
             'PIN 변경',
             'PIN 이 변경되었습니다.',
             [
-              {text: '확인', onPress: ()=>this.props.navigation.goBack()}
+              {text: '확인', onPress: ()=>props.navigation.goBack()}
             ]);
         }else{
-          this.showSnackbar('틀린 PIN 입니다.');
+          showSnackbar('틀린 PIN 입니다.');
         }
       }else{
-        this.showSnackbar('새 PIN 과 PIN 확인 값이 다릅니다.');
+        showSnackbar('새 PIN 과 PIN 확인 값이 다릅니다.');
       }
     }else{
-      this.showSnackbar('PIN은 6자리여야 합니다.');
+      showSnackbar('PIN은 6자리여야 합니다.');
     }
-  }
+  };
+
+  const idToNewPin = 'idToNewPin';
+  const idToNewPinCheck = 'idToNewPinCheck';
+  const idChange = 'idChange';
+
+  let keyboardToolbar = (Platform.OS == 'ios') ? (
+    <View>
+      <InputAccessoryView nativeID={idToNewPin}>
+        <Button
+          onPress={() => newPinInput.current.focus()}
+          title="새 PIN(6자) 입력"
+        />
+      </InputAccessoryView>
+      <InputAccessoryView nativeID={idToNewPinCheck}>
+        <Button
+          onPress={() => newPinCheckInput.current.focus()}
+          title="새 PIN 확인(6자) 입력"
+        />
+      </InputAccessoryView>
+      <InputAccessoryView nativeID={idChange}>
+        <Button
+          onPress={() => changePin()}
+          title="PIN 변경"
+        />
+      </InputAccessoryView>
+    </View>) : (<View></View>);
+  return(
+    <KeyboardAvoidingView behavior="padding" enabled style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <ListItem>
+          <TextInput style={{color: colors.text}} placeholder='기존 PIN 입력' 
+            returnkeyType='next' keyboardType='number-pad'
+            secureTextEntry={ true } autocorrect={ false } ref={currentPinInput}
+            onSubmitEditing={ () => newPinInput.current.focus() } maxLength={6}
+            onChangeText={(value)=>setCurrentPin(value)}
+            inputAccessoryViewID={idToNewPin}/>
+        </ListItem>
+        <ListItem>
+          <TextInput style={{color: colors.text}} placeholder='새 PIN 입력(6자)' 
+            returnkeyType='next' keyboardType='number-pad'
+            maxLength={6} secureTextEntry={ true } autocorrect={ false } ref={newPinInput}
+            onSubmitEditing={ () => newPinCheckInput.current.focus() }
+            onChangeText={(value)=>setNewPin(value)}
+            inputAccessoryViewID={idToNewPinCheck}/>
+        </ListItem>
+        <ListItem>
+          <TextInput style={{color: colors.text}} placeholder='새 PIN 확인(6자)' 
+            returnkeyType='go' keyboardType='number-pad'
+            maxLength={6} secureTextEntry={ true } autocorrect={ false } ref={newPinCheckInput}
+            onSubmitEditing={ () => changePin() }
+            onChangeText={(value)=>setNewPinCheck(value)}
+            inputAccessoryViewID={idChange}/>
+        </ListItem>
+      </View>
+      <View style={{flex: 0, flexDirection: 'row', backgroundColor: 'white',
+        height: 50, width: '100%', marginBottom: 16}}>
+        <ListItem style={{flex: 1, alignItems: 'center'}}
+          onPress={()=>props.navigation.goBack()}>
+          <ThemedText>취소</ThemedText>
+        </ListItem>
+        <ListItem style={{flex: 1, alignItems: 'center'}}
+          onPress={()=>changePin()}>
+          <ThemedText>복구</ThemedText>
+        </ListItem>
+      </View>
+      <SnackBar visible={snackbar} textMessage={msg}/>
+      {keyboardToolbar}
+    </KeyboardAvoidingView>
+  );
 }
