b834eed0b5cdd01c95638a13e287a4fd4c0fd1c4
sukso96100 <sukso96100@gmail.com>
2020-06-04 16:25:33 +0900

    [feature/update-modal] 로드 중일 때 로딩 스피너 표시

--- src/components/whatsNewModal.js
+++ src/components/whatsNewModal.js
@@ -2,15 +2,18 @@ import UpdateInfo from '../updateInfo';
 import {InfoModal} from './infoModal';
 import * as WebBrowser from 'expo-web-browser';
 import {ThemedText} from './components';
-import {AsyncStorage, View} from 'react-native';
+import {AsyncStorage, View, ActivityIndicator} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import moment from 'moment';
+import BuildConfigs from '../config';
+
 
 export function WhatsNewModal(props){
   const allReleaseUrl = 'https://github.com/s-owl/skhu-app/releases';
   const releaseNoteUrl = `https://api.github.com/repos/s-owl/skhu-app/releases/tags/${UpdateInfo.ReleaseTag}`;
-  const [releaseNoteContent, setReleaseNoteContent] = useState('(불러오는 중...)');
-  const [publishedAt, setPublishedAt] = ('');
+  const [releaseNoteContent, setReleaseNoteContent] = useState('');
+  const [publishedAt, setPublishedAt] = useState('');
+  const [isLoading, setLoading] = useState(false);
   const loadErrMsg = '업데이트 내역을 GitHub 에서 불러오지 못했습니다. 닫았다가 열어 다시 시도하거나 아래 "모든 내역 보기" 버튼을 이용하세요.';
   let info;
   if(props.isAuto){
@@ -24,17 +27,43 @@ export function WhatsNewModal(props){
   useEffect(()=>{
     (async()=>{
       if(props.visible){
+        setLoading(true);
+        let body = '';
         try{
           const res = await fetch(releaseNoteUrl);
           const data = await res.json();
-          setReleaseNoteContent(data.body);
+          body = data.body;
           setPublishedAt(moment.utc(data.published_at).local().format('YYYY.MM.DD hh:mm:ss'));
         }catch(e){
-          setReleaseNoteContent(loadErrMsg);
+          body = loadErrMsg;
         }
+        setReleaseNoteContent(body);
+        setLoading(false);
       }
     })();
   }, [props.visible]);
+
+  let content = isLoading? (
+    <View style={{padding: 8, alignItems: 'center'}}>
+      <ActivityIndicator size="large" color={BuildConfigs.primaryColor} />
+    </View>
+  ):(
+    <View>
+      <View style={{padding: 8, alignItems: 'center', flex: 0, marginBottom: 8}}>
+        <ThemedText>
+          {`${UpdateInfo.AppVersion} / OTA - ${UpdateInfo.OtaDeployedAt}`}
+        </ThemedText>
+        <ThemedText>
+          {publishedAt}
+        </ThemedText>
+      </View>
+      <ThemedText>
+        {releaseNoteContent}
+      </ThemedText>
+      {info}
+    </View>
+  );
+
   return(
     <InfoModal
       visible={props.visible}
@@ -48,18 +77,7 @@ export function WhatsNewModal(props){
           props.onClose();
           await AsyncStorage.setItem(UpdateInfo.ReleaseIdKey, (true).toString());}}
       ]}>
-      <View style={{padding: 8, alignItems: 'center', flex: 0}}>
-        <ThemedText style={{padding: 8}}>
-          {`${UpdateInfo.AppVersion} / OTA - ${UpdateInfo.OtaDeployedAt}`}
-        </ThemedText>
-        <ThemedText>
-          {publishedAt}
-        </ThemedText>
-      </View>
-      <ThemedText>
-        {releaseNoteContent}
-      </ThemedText>
-      {info}
+      {content}
     </InfoModal>
   );
 }
