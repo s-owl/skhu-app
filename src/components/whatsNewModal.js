443ad1de7965e07205a012d05e68fba69eb34f07
sukso96100 <sukso96100@gmail.com>
2020-06-04 16:07:30 +0900

    [feature/update-modal] 업데이트 내역 GitHub 에서 불러오도록 수정

--- src/components/whatsNewModal.js
+++ src/components/whatsNewModal.js
@@ -3,9 +3,15 @@ import {InfoModal} from './infoModal';
 import * as WebBrowser from 'expo-web-browser';
 import {ThemedText} from './components';
 import {AsyncStorage, View} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import moment from 'moment';
 
 export function WhatsNewModal(props){
+  const allReleaseUrl = 'https://github.com/s-owl/skhu-app/releases';
+  const releaseNoteUrl = `https://api.github.com/repos/s-owl/skhu-app/releases/tags/${UpdateInfo.ReleaseTag}`;
+  const [releaseNoteContent, setReleaseNoteContent] = useState('(불러오는 중...)');
+  const [publishedAt, setPublishedAt] = ('');
+  const loadErrMsg = '업데이트 내역을 GitHub 에서 불러오지 못했습니다. 닫았다가 열어 다시 시도하거나 아래 "모든 내역 보기" 버튼을 이용하세요.';
   let info;
   if(props.isAuto){
     info = (
@@ -14,6 +20,21 @@ export function WhatsNewModal(props){
   }else{
     info = (<View></View>);
   }
+
+  useEffect(()=>{
+    (async()=>{
+      if(props.visible){
+        try{
+          const res = await fetch(releaseNoteUrl);
+          const data = await res.json();
+          setReleaseNoteContent(data.body);
+          setPublishedAt(moment.utc(data.published_at).local().format('YYYY.MM.DD hh:mm:ss'));
+        }catch(e){
+          setReleaseNoteContent(loadErrMsg);
+        }
+      }
+    })();
+  }, [props.visible]);
   return(
     <InfoModal
       visible={props.visible}
@@ -21,7 +42,7 @@ export function WhatsNewModal(props){
       title='업데이트 내역'
       buttons={[
         {label: '모든 내역 보기', onPress: async()=>{
-          await WebBrowser.openBrowserAsync(UpdateInfo.AllReleaseUrl);
+          await WebBrowser.openBrowserAsync(allReleaseUrl);
         }},
         {label: '닫기', onPress: async()=>{
           props.onClose();
@@ -31,9 +52,12 @@ export function WhatsNewModal(props){
         <ThemedText style={{padding: 8}}>
           {`${UpdateInfo.AppVersion} / OTA - ${UpdateInfo.OtaDeployedAt}`}
         </ThemedText>
+        <ThemedText>
+          {publishedAt}
+        </ThemedText>
       </View>
       <ThemedText>
-        {UpdateInfo.ReleaseNoteSummaryContent}
+        {releaseNoteContent}
       </ThemedText>
       {info}
     </InfoModal>
--- src/updateInfo.js
+++ src/updateInfo.js
@@ -3,9 +3,6 @@ import Constants from 'expo-constants';
 export default class UpdateInfo{
     static AppVersion = Constants.manifest.version;
     static OtaDeployedAt = '2020.03.30';
-    static ReleaseNoteSummaryContent = `
-    - 앱 업데이트 시 앱에서 업데이트 내역 보여주도록 구현
-    `
-    static AllReleaseUrl = 'https://github.com/s-owl/skhu-app/releases';
+    static ReleaseTag = 'OTA-2020.03.30'; // Name of Release on GitHub Repo
     static ReleaseIdKey =  `${this.AppVersion}_${this.OtaDeployedAt}`;
 }
