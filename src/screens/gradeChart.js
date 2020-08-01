96f371aa8f0383373d4ed07e2fa0688f6d1d4b84
sukso96100 <sukso96100@gmail.com>
2020-08-02 01:06:35 +0900

    [feature/grade-cal]차트 기능 추가

--- src/screens/gradeChart.js
+++ src/screens/gradeChart.js
@@ -1,15 +1,35 @@
-import React, {useEffect, useState, } from 'react';
+import React, {useEffect, useState} from 'react';
 import ListItem from '../components/listitem';
-import {View, Text, FlatList, ActivityIndicator} from 'react-native';
+import {View, FlatList, ActivityIndicator, Dimensions} from 'react-native';
 import ForestApi from '../tools/apis';
 import BuildConfigs from '../config';
+import {useTheme} from '@react-navigation/native';
 import {ThemedText, Collapsible} from '../components/components';
-
+import { LineChart} from "react-native-chart-kit";
 export default function GradeChart(props){
-
+  const {colors} = useTheme();
   const [details, setDetails] = useState([]);
-  const [summary, setSummary] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [chartData, setChartData] = useState({
+    labels: ["1","2","3","4","5"],
+    datasets: [
+      {
+        data: [1,2,3,4,5],
+        color: () => colors.primary, // optional
+        strokeWidth: 2 // optional
+      }
+    ],
+    legend: ["평점"] // optional
+  });
+  const screenWidth = Dimensions.get("window").width * 0.95;
+  const chartConfig = {
+    backgroundGradientFrom: colors.background,
+    backgroundGradientTo: colors.background,
+    color: () => colors.text,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
 
   useEffect(()=>{
     (async ()=>{
@@ -19,7 +39,18 @@ export default function GradeChart(props){
         const data = await gradeCert.json();        
         let groups = [];
         let detailsData = [];
-        let summaryData = [];
+        let summaryData = {
+          labels: [],
+          datasets: [
+            {
+              data: [],
+              color: () => colors.primary, // optional
+              strokeWidth: 2 // optional
+            }
+          ],
+          legend: ["평점"] // optional
+        };
+      
         for(const item of data.details){
           if(item.subject.includes("백분율")){
             detailsData.push({
@@ -32,12 +63,8 @@ export default function GradeChart(props){
               subjects: groups
             });
             let splited = item.subject.split(" ");
-            summaryData.push({
-              year: item.year,
-              semester: item.semester,
-              earned: Number(splited[1]),
-              grade: Number(splited[3])
-            })
+            summaryData.labels.push(`${item.year}-${item.semester}`);
+            summaryData.datasets[0].data.push(Number(splited[3]));
             groups = [];
           }else{
             groups.push(item);
@@ -45,7 +72,7 @@ export default function GradeChart(props){
         }
         
         setDetails(detailsData);
-        setSummary(summaryData);
+        setChartData(summaryData);
         setIsLoading(false);
       }
     })();
@@ -62,6 +89,12 @@ export default function GradeChart(props){
       <FlatList style={{height: '100%'}}
         ListHeaderComponent={()=>(
           <View>
+            <LineChart
+              data={chartData}
+              width={screenWidth}
+              height={220}
+              chartConfig={chartConfig}
+            />
           </View>
         )}
         
