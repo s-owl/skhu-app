f5b40407e322666cc84ebe7fc3b2da89a3093d03
sukso96100 <sukso96100@gmail.com>
2020-08-17 15:30:27 +0900

    [release/ota-2020.08.17] OTA 업데이트 준비

--- src/updateInfo.js
+++ src/updateInfo.js
@@ -2,7 +2,7 @@ import Constants from 'expo-constants';
 
 export default class UpdateInfo{
     static AppVersion = Constants.manifest.version;
-    static OtaDeployedAt = '2020.08.04';
-    static ReleaseTag = 'OTA-2020.08.04'; // Name of Release on GitHub Repo
+    static OtaDeployedAt = '2020.08.17';
+    static ReleaseTag = 'OTA-2020.08.17'; // Name of Release on GitHub Repo
     static ReleaseIdKey =  `${this.AppVersion}_${this.OtaDeployedAt}`;
 }
