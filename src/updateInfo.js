24207276cc204b62fd03d8488a9fdae7714d7579
Youngbin Han <sukso96100@gmail.com>
2021-01-31 21:26:45 +0900

    1.31 OTA 준비

--- src/updateInfo.js
+++ src/updateInfo.js
@@ -2,7 +2,7 @@ import Constants from 'expo-constants';
 
 export default class UpdateInfo{
     static AppVersion = Constants.manifest.version;
-    static OtaDeployedAt = '2020.09.19';
-    static ReleaseTag = 'OTA-2020.09.19'; // Name of Release on GitHub Repo
+    static OtaDeployedAt = '2021.01.31';
+    static ReleaseTag = 'OTA-2021.01.31'; // Name of Release on GitHub Repo
     static ReleaseIdKey =  `${this.AppVersion}_${this.OtaDeployedAt}`;
 }
